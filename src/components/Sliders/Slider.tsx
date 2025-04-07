011073d5fe826bb2bb6b331a840a1b402ab3a572
AnithaVangavarapu <anitha.vangavarapu@quadone.com>
2025-04-07 10:05:56 +0530

    previous button added

--- src/components/Sliders/Slider.tsx
+++ src/components/Sliders/Slider.tsx
@@ -10,7 +10,13 @@ const Slider = () => {
     "outstanding",
   ];
   // const values = ["1", "2", "3", "4", "5"];
-
+  const handlePrevious = () => {
+    if (status === 0) {
+      return;
+    } else {
+      setStatus(status - 1);
+    }
+  };
   const handleNext = () => {
     if (status === values.length - 1) {
       return;
@@ -23,7 +29,7 @@ const Slider = () => {
   return (
     <div className="mx-10 ">
       <div
-        className={`flex justify-between border rounded-md border-gray-400 ease-in-out`}
+        className={`flex justify-between border rounded-md border-gray-400 `}
         style={{
           background: `linear-gradient(to right, #3b82f6 ${
             status * (100 / (values.length - 1))
@@ -36,7 +42,7 @@ const Slider = () => {
             className={` flex justify-center items-center h-2 w-fit `}
           >
             <Circle
-              fill={status === idx || idx <= status ? "red" : "white"}
+              fill={status === idx || idx <= status ? "#3b82f6" : "white"}
               color="blue"
               className="h-6 stroke-1"
             />
@@ -51,9 +57,18 @@ const Slider = () => {
           </div>
         ))}
       </div>
-      <div>
-        <button className="border rounded-md px-1" onClick={handleNext}>
-          next
+      <div className="flex justify-center">
+        <button
+          className="border rounded-md px-1 bg-blue-500 text-white mt-2 mx-4"
+          onClick={handlePrevious}
+        >
+          Prev
+        </button>
+        <button
+          className="border rounded-md px-1 bg-blue-500 text-white mt-2"
+          onClick={handleNext}
+        >
+          Next
         </button>
       </div>
     </div>
